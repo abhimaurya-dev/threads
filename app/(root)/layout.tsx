e08ec49e05ef4d3883648bf5ba7d90ad22be5b93
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2024-12-23 18:26:49 +0530

    created signin and signup using clerk and also cretaed frontend of onboarding component along with navigation

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Threads",
+  description: "A nextjs 13 Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="flex flex-row">
+          <ClerkProvider>
+            <Topbar />
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+            <Bottombar />
+          </ClerkProvider>
+        </main>
+      </body>
+    </html>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
+  serverExternalPackages: ["mongoose"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
